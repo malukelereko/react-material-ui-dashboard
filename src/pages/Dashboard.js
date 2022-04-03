93cbb26e918ceaa0bba7213200c5e0d08541a462
maluke lereko <lerekomaduke@gmail.com>
2022-04-03 17:05:20 +0200

    Dashboard header

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Paper, Typography, Button, Box, Grid } from '@mui/material'
+import { Select, FormControl, MenuItem, InputLabel } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
-const StatCard = ({ title, value, percentage}) => {
+const StatCard = ({ title, value, percentage, period }) => {
 
     return (
         <div>
@@ -23,7 +24,7 @@ const StatCard = ({ title, value, percentage}) => {
                     {percentage}
                 </Box>
                 <Box sx={{ color: 'text.secondary', display: 'inline', fontSize: 14 }}>
-                vs. last week
+                vs. {period}
                 </Box>
             </Paper>
         </div>
@@ -31,39 +32,64 @@ const StatCard = ({ title, value, percentage}) => {
 }
 
 const Dashboard = () => {
-  return (
-    <div>
-        <Paper sx={{ p: 3}}>
-            <Typography variant='h4' mt={4}>
-                Welcome to Maluke's dashboard!
-            </Typography>
-            <Box sx={{ width: 1/2, mt: 2, mb: 2 }}>
-                <Typography variant='body1'>
-                    React and Material UI dashboard that uses ChartJS for Graph library, ready to get you started with your project;
-                </Typography>
+
+    const [period, setPeriod] = useState('')
+
+    const handleChange = (event) => {
+        setPeriod(event.target.value);
+      };
+    return (
+        <div>
+            {/* Heading */}
+            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-between', mt: 2, mb: 2 }}>
+                <Typography variant='h3'>Dashboard</Typography>
+                <FormControl sx={{ minWidth: 100 }}>
+                    <InputLabel id="period">Period</InputLabel>
+                    <Select
+                    labelId="period"
+                    id="period-select"
+                    value={period}
+                    label="Period"
+                    onChange={handleChange}
+                    >
+                        <MenuItem value={"Last Week"}>Last Week</MenuItem>
+                        <MenuItem value={"Last Month"}>Last Month</MenuItem>
+                        <MenuItem value={"Last Year"}>Last Year</MenuItem>
+                    </Select>
+                </FormControl>
             </Box>
-            <Button variant="contained" sx={{mb: 3, }} endIcon={<ArrowForwardIcon />}>Get Started</Button>
-        </Paper>
 
-        <Grid container spacing={2}>
-            <Grid item lg={3} md={6} sm={12}>
-                <StatCard title={"Revenue"} value={"M 14,000"} percentage={"+11.97%"} />
-            </Grid>
+            <Paper sx={{ p: 3}}>
+                <Typography variant='h4' mt={4}>
+                    Welcome to Maluke's dashboard!
+                </Typography>
+                <Box sx={{ width: 1/2, mt: 2, mb: 2 }}>
+                    <Typography variant='body1'>
+                        React and Material UI dashboard that uses ChartJS for Graph library, ready to get you started with your project;
+                    </Typography>
+                </Box>
+                <Button variant="contained" sx={{mb: 3, }} endIcon={<ArrowForwardIcon />}>Get Started</Button>
+            </Paper>
 
-            <Grid item lg={3} md={6} sm={12}>
-                <StatCard title={"Average Transaction"} value={"M 325"} percentage={"+11.97%"} />
-            </Grid>
+            <Grid container spacing={2}>
+                <Grid item lg={3} md={6} sm={12}>
+                    <StatCard title={"Revenue"} value={"M 14,000"} percentage={"+11.97%"} period={period}/>
+                </Grid>
 
-            <Grid item lg={3} md={6} sm={12}>
-                <StatCard title={"Registrations"} value={"2,001"} percentage={"+11.97%"} />
-            </Grid>
+                <Grid item lg={3} md={6} sm={12}>
+                    <StatCard title={"Average Transaction"} value={"M 325"} percentage={"+11.97%"} period={period}/>
+                </Grid>
 
-            <Grid item lg={3} md={6} sm={12}>
-                <StatCard title={"Convertion Rate"} value={"55%"} percentage={"+11.97%"} />
+                <Grid item lg={3} md={6} sm={12}>
+                    <StatCard title={"Registrations"} value={"2,001"} percentage={"+11.97%"} period={period}/>
+                </Grid>
+
+                <Grid item lg={3} md={6} sm={12}>
+                    <StatCard title={"Convertion Rate"} value={"55%"} percentage={"+11.97%"} period={period}/>
+                </Grid>
             </Grid>
-        </Grid>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Dashboard
