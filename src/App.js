6ac3803c8fb347392bb7bf62652e78da51f6fa37
maluke lereko <lerekomaduke@gmail.com>
2022-04-03 10:02:32 +0200

    first commit for project layout and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Box, CssBaseline } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 
 function App() {
+
+  // Initialise Theme
+const theme = createTheme();
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path='/' element={<h1>Hello world</h1>}></Route>
+          </Routes>
+        </Router>
+      </Box>
+    </ThemeProvider>
   );
 }
 
